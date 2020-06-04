e099468b090fbb97dbd8d63a44a1a97712b7c1fb
egaimachine <ssgokulsiva@gmail.com>
2020-06-04 16:04:06 +0530

    commit

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow() {
     win = new Browserwindow({
         width: 800,
         height: 880,
-        icon: path.join(__dirname, 'app/assets/logo/emoy_logo.png'),
+        icon: path.join(__dirname, 'app/assets/logo/logo.png'),
         webPreferences: {
             nodeIntegration: true,
             webviewTag: true,
